12d2795285c2da67e124672c3bc242f88c2bc745
long.vu <vuduclong10a1cma@gmail.com>
2019-09-27 21:03:20 +0700

    update

--- js-practices/Practice3/generatePractice.js
+++ js-practices/Practice3/generatePractice.js
@@ -8,26 +8,18 @@ function generateOutput(input, target){
     return -1;
   }
 }
-
+// bai nay co cach tu tao them 2 test cho target o dau va o cuoi roi cho them vao arr nhung e thay hoi tu nen khong lam theo cach do :v 
 function generateArray(testLengthArray,index){
   return Array.from({length: testLengthArray[index]}, () => Math.floor(Math.random() * 300));
 }
 
 function generate(testLengthArray){
-  // return Array.from({length : testLengthArray.length})
-  //   .map(item => ({
-  //     input: Array.from({length: item}).map(item => []),
-  //     target: 0,
-  //     output: -1
-  //   })
-  // ); 
   // Remove this line and change to your own algorithm
   let arr = [];
   for (let index = 0; index < testLengthArray.length; index++) {
     let target = testLengthArray[index];
     let input = generateArray(testLengthArray,index);
     let output = generateOutput(input,target);
-    
     let testData = {
       input: input,
       output: output,
